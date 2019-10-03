dd5844d5ad75e6657ec10c1ddd9cfe0c2e57e895
Laris Mahmutaj <laris.mahmutaj@gmail.com>
2019-10-03 17:58:03 +0200

    Add and delete contact UI

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
+import ContactContext from '../../context/contact/contactContext'
 
 const ContactItem = ({ contact }) => {
+  const contactContext = useContext(ContactContext)
+  const { deleteContact } = contactContext
+
   const { id, name, email, phone, type } = contact
+
+  const onDelete = () => {
+    deleteContact(id)
+  }
+
   return (
     <div className='card bg-light'>
       <h3 className='text-primary text-left'>
         {name}{' '}
         <span
           style={{ float: 'right' }}
-          className={'badge ' + (type === 'professional' ? 'badge-success' : 'badge-primary')}
+          className={
+            'badge ' +
+            (type === 'professional' ? 'badge-success' : 'badge-primary')
+          }
         >
           {type.charAt(0).toUpperCase() + type.slice(1)}
         </span>
@@ -28,7 +40,9 @@ const ContactItem = ({ contact }) => {
       </ul>
       <p>
         <button className='btn btn-dark btn-sm'>Edit</button>
-        <button className='btn btn-danger btn-sm'>Delete</button>
+        <button className='btn btn-danger btn-sm' onClick={onDelete}>
+          Delete
+        </button>
       </p>
     </div>
   )
